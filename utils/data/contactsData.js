001e738a76a61d5a06507ade7ca2df8c73214bfb
MohamedIjlal27 <mohamedijlal27@gmail.com>
2024-05-22 11:16:46 +0530

    Contact Details Updated

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'mohamedijlal27@outlook.com',
+    phone: '0727920628',
+    address: '48/123c 1/1, Dharmasala Mawatha, Obeysekarapura Rajagiriya, Colombo',
+    github: 'https://github.com/MohamedIjlal27',
+    facebook: '',
+    linkedIn: 'http://www.linkedin.com/in/mohamed-ijlal',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: ""
 }
