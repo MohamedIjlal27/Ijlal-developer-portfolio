efb77ee260ad9284ab31b91afdb0f9e03030503f
MohamedIjlal27 <mohamedijlal27@gmail.com>
2024-06-10 10:12:33 +0530

    Correction

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,37 +1,37 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer(Mobile & Web)',
+    title: "Software Engineer(Mobile & Web)",
     company: "Sankaraa Tech Pvt Ltd.",
     workmode: "Onsite",
-    duration: "(May 2024 - Present)"
+    duration: "(May 2024 - Present)",
   },
   {
     id: 2,
-    title: 'Software Engineer(Mobile & Web)',
+    title: "Software Engineer(Mobile & Web)",
     company: " HotCat Technology Sri Lanka.",
     workmode: "Remote until August 2023",
-    duration: "(Jan 2023 - May 2024)"
+    duration: "(Jan 2023 - May 2024)",
   },
   {
     id: 3,
-    title: 'Associate Software Engineer',
+    title: "Associate Software Engineer",
     company: " HotCat Technology Sri Lanka.",
     workmode: "Remote",
-    duration: "(March 2022 - Jan 20232)"
+    duration: "(March 2022 - Jan 2023)",
   },
   {
     id: 4,
-    title: 'Trainee Software Engineer',
+    title: "Trainee Software Engineer",
     company: " HotCat Technology Sri Lanka.",
     workmode: "Remote",
-    duration: "(Jan 2022- March 2022)"
+    duration: "(Jan 2022- March 2022)",
   },
   {
     id: 5,
-    title: 'Freelance Dot Net Developer(VB.NET)',
+    title: "Freelance Dot Net Developer(VB.NET)",
     company: "Pakistan",
     workmode: "Remote",
-    duration: "(2021-2022)"
-  }
-]
+    duration: "(2021-2022)",
+  },
+];
