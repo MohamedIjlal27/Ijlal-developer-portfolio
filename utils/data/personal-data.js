0d6f3bc3c509f363a0f12980ef64d2865380c0eb
Mohamed Ijlal <mohamedijlal27@gmail.com>
2024-08-20 07:55:33 +0530

    Cv Location Changed

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,19 @@
 export const personalData = {
   name: "Mohamed Ijlal",
-  profile: '/profile.png',
+  profile: "/profile.png",
   designation: "Software Developer",
-  description: "My name is Mohamed Ijlal. I am a self-motivated software developer with over 2 years of experience in building enterprise grade & commercial business applications. I excel in crafting high-quality mobile & web applications. My skill set includes strong research and analytical capabilities, complemented by a proactive attitude and excellent communication skills. My positive can-do attitude and unwavering commitment to quality propel my work forward. .",
-  email: 'mohamedijlal27@outlook.com',
-  phone: '+94727920628',
-  address: 'Colombo Sri Lanka ',
-  github: 'https://github.com/MohamedIjlal27',
-  facebook: '',
-  linkedIn: 'http://www.linkedin.com/in/mohamed-ijlal',
-  twitter: '',
-  stackOverflow: '',
+  description:
+    "My name is Mohamed Ijlal. I am a self-motivated software developer with over 2 years of experience in building enterprise grade & commercial business applications. I excel in crafting high-quality mobile & web applications. My skill set includes strong research and analytical capabilities, complemented by a proactive attitude and excellent communication skills. My positive can-do attitude and unwavering commitment to quality propel my work forward. .",
+  email: "mohamedijlal27@outlook.com",
+  phone: "+94727920628",
+  address: "Colombo Sri Lanka ",
+  github: "https://github.com/MohamedIjlal27",
+  facebook: "",
+  linkedIn: "http://www.linkedin.com/in/mohamed-ijlal",
+  twitter: "",
+  stackOverflow: "",
   leetcode: "",
   devUsername: "",
-  resume: "https://drive.google.com/file/d/19gOlzK4dyC8DZzOLUzDmlIdf5_tbzKpw/view?usp=sharing"
-}
+  resume:
+    "https://drive.google.com/file/d/1f6giXJ1RWJCGcPnc4370TIMIHhXEKsjW/view?usp=drive_link",
+};
