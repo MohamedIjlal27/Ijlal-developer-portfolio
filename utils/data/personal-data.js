5ef3cc2ec045abfef38ff9bfd08633c1423488ec
MohamedIjlal27 <mohamedijlal27@gmail.com>
2025-05-08 12:58:13 +0530

    CV Updated

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -14,7 +14,7 @@ export const personalData = {
   stackOverflow: "",
   leetcode: "",
   devUsername: "",
-  resume: "https://drive.google.com/file/d/1W1cv8A4yH7tPFqh6koGPO1ZXQ175ChUv/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1Nmbj7GWNscG84mWwMJUeXfD-GV3gu6Dg/view?usp=sharing"
   // "https://drive.google.com/file/d/1mFJIoPD0tVMvE3jALudDixtv8zP63fmz/view?usp=sharing",
   // "https://drive.google.com/file/d/1mFJIoPD0tVMvE3jALudDixtv8zP63fmz/view?usp=sharing",
   //"https://drive.google.com/file/d/1gLKr1G7T3QsnhMBznI0WfPgOgKplKVoc/view?usp=sharing",
