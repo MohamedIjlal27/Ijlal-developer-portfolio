6ad88c8ace9a373ee0cc90d38c7951bd8feec096
MohamedIjlal27 <mohamedijlal27@gmail.com>
2025-05-15 16:25:20 +0530

    Fixed the project cards bug

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,6 +1,5 @@
 import { projectsData } from '@/utils/data/projects-data';
 import ProjectCard from './project-card';
-import Marquee from "react-fast-marquee";
 
 const Projects = () => {
   return (
@@ -16,27 +15,19 @@ const Projects = () => {
       </div>
 
       <div className="pt-24">
-        <div className="w-full">
-          <Marquee
-            gradient={false}
-            speed={80}
-            pauseOnHover={true}
-            pauseOnClick={true}
-            delay={0}
-            play={true}
-            direction="left"
-          >
+        <div className="container mx-auto px-4">
+          <div className="grid gap-8 max-w-4xl mx-auto">
             {projectsData.map((project, index) => (
               <div
                 key={index}
-                className="w-[400px] min-w-fit h-fit flex flex-col items-center justify-center transition-all duration-500 m-3 sm:m-5 rounded-lg group relative hover:scale-[1.15] cursor-pointer"
+                className="w-full transition-all duration-500 rounded-lg group relative hover:scale-[1.02] cursor-pointer"
               >
                 <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)]">
                   <ProjectCard project={project} />
                 </div>
               </div>
             ))}
-          </Marquee>
+          </div>
         </div>
       </div>
     </div>
